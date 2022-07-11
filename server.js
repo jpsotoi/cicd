225f57c44ec2dc675ca7229bfbf69f6a1eaf9684
Juan Pablo <juan.soto@coppel.com>
2022-07-11 15:43:15 -0600

    Se cambia nombre a Borre

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Beja riatudo!');
+  res.send('Hello Borre riatudo!');
 });
 
 app.listen(PORT, HOST);
